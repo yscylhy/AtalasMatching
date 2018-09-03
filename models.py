57ce6a0bbddc366c7c7567286ab23830303e394e
yscylhy <lianghaoyi91@gmail.com>
2018-09-02 23:49:21 -0400

    implement the regression approach for detection

--- models.py
+++ models.py
@@ -15,8 +15,7 @@ import matplotlib
 from itertools import product
 from functools import partial
 import scipy.misc
-
-matplotlib.use('Agg')
+import matplotlib.patches as patches
 
 
 def w_binary_crossentropy(y_true, y_pred):
@@ -46,10 +45,9 @@ class DGBoxNet:
         pool2 = MaxPooling2D((2, 2), strides=(2, 2))(conv2)
         conv3 = Conv2D(128, (5, 5), activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
         pool3 = MaxPooling2D((2, 2), strides=(2, 2))(conv3)
-        conv4 = Conv2D(256, (5, 5), activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
-        drop4 = Dropout(0.1)(conv4)
-        outputs = Conv2D(4, (1, 1), activation='softmax', padding='same', kernel_initializer='he_normal')(drop4)
-
+        drop3 = Dropout(0.4)(pool3)
+        outputs = Conv2D(4, (35, 25), activation='relu', padding='valid', kernel_initializer='he_normal')(drop3)
+        outputs = Flatten()(outputs)
         model = Model(input=inputs, output=outputs)
 
         return model
@@ -60,15 +58,16 @@ class DGBoxNet:
         model.summary()
 
         checkpointer = ModelCheckpoint(filepath=os.path.join(self.model_path, 'seg_best_weights.h5'),
-                                       verbose=0, monitor='loss', save_best_only=True)
+                                       verbose=0, monitor='val_loss', save_best_only=True)
         optimizer = Adam(lr=lr)
         model.compile(optimizer=optimizer, loss=mean_squared_error, metrics=['accuracy'])
         hist = model.fit(x_data, y_data, epochs=N_epoch, batch_size=batch_size, verbose=2, shuffle=True,
                        validation_split=validation_split, callbacks=[checkpointer])
+
         fig = plt.figure()
         plt.plot(hist.history['loss'], '-o')
-        # plt.plot(hist.history['val_loss'], 'g-o')
-        # plt.legend(['loss', 'val_loss'])
+        plt.plot(hist.history['val_loss'], 'g-o')
+        plt.legend(['loss', 'val_loss'])
         fig.savefig('seg_train_convergence.png', format='png', bbox_inches='tight', dpi=900)
         plt.close()
         model.save_weights(os.path.join(self.model_path, 'seg_last_weights.h5'), overwrite=True)
@@ -83,11 +82,18 @@ class DGBoxNet:
         model.load_weights(os.path.join(self.model_path, model_name))
 
         predict_results = model.predict(img, verbose=1)
-        # prob_map = np.squeeze(predict_results[:, :, :, 1])
-        # fig = plt.figure()
-        # plt.imshow(prob_map, cmap='plasma')  # default is 'viridis'
-        # plt.colorbar()
-        # fig.savefig('seg_prob_map.png', format='png', bbox_inches='tight', dpi=900)
-        # plt.close()
-        # scipy.misc.imsave('outfile.jpg', (prob_map*200).astype(np.uint8))
+        fig, ax = plt.subplots(1)
+        ax.imshow(img[0, :, :, 0])
+        start_row = predict_results[0, 0]
+        start_col = predict_results[0, 1]
+        row_len = predict_results[0, 2]
+        col_len = predict_results[0, 3]
+
+        rect = patches.Rectangle((start_col, start_row), col_len, row_len,
+                                 linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+
+        fig.savefig('predict_results.png', format='png', bbox_inches='tight', dpi=900)
+        plt.close()
+
         return predict_results
